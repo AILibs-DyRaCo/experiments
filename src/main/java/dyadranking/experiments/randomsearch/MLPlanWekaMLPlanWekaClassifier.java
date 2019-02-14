d07fc618fdd19b7ece8338e777872490be3e65fb
Mirko Juergens <mirkoj@mail.upb.de>
2019-02-14 13:39:44 +0100

    adjustments to performancesample generator

---
+++ src/main/java/dyadranking/experiments/randomsearch/MLPlanWekaMLPlanWekaClassifier.java
@@ -0,0 +1,16 @@
+package dyadranking.experiments.randomsearch;
+
+import java.io.IOException;
+
+import de.upb.crc901.mlplan.multiclass.wekamlplan.weka.WekaMLPlanWekaClassifier;
+
+public class MLPlanWekaMLPlanWekaClassifier extends WekaMLPlanWekaClassifier {
+
+	public MLPlanWekaMLPlanWekaClassifier() throws IOException {
+		super();
+		this.comonents
+	}
+
+
+
+}
--- src/main/java/dyadranking/experiments/randomsearch/PerformanceSampleGenerator.java
+++ src/main/java/dyadranking/experiments/randomsearch/PerformanceSampleGenerator.java
@@ -1,11 +1,17 @@
 package dyadranking.experiments.randomsearch;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.aeonbits.owner.ConfigFactory;
 
-import de.upb.crc901.mlpipeline_evaluation.CacheEvaluatorMeasureBridge;
-import de.upb.crc901.mlpipeline_evaluation.PerformanceDBAdapter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import de.upb.crc901.mlplan.multiclass.wekamlplan.MLPlanWekaClassifier;
 import de.upb.crc901.mlplan.multiclass.wekamlplan.weka.WEKAPipelineFactory;
-import de.upb.crc901.mlplan.multiclass.wekamlplan.weka.WekaMLPlanWekaClassifier;
 import de.upb.crc901.mlplan.multiclass.wekamlplan.weka.model.MLPipeline;
 import hasco.core.Util;
 import hasco.model.ComponentInstance;
@@ -13,11 +19,13 @@ import jaicore.basic.SQLAdapter;
 import jaicore.ml.cache.ReproducibleInstances;
 import jaicore.ml.core.evaluation.measure.singlelabel.ZeroOneLoss;
 import jaicore.ml.evaluation.evaluators.weka.MonteCarloCrossValidationEvaluator;
+import jaicore.ml.evaluation.evaluators.weka.SimpleEvaluatorMeasureBridge;
 import jaicore.planning.graphgenerators.task.tfd.TFDNode;
 import jaicore.search.algorithms.standard.random.RandomSearch;
 import jaicore.search.core.interfaces.GraphGenerator;
 import jaicore.search.model.other.SearchGraphPath;
 import jaicore.search.model.probleminputs.GraphSearchInput;
+
 /**
  * Simple class that can be used to generate performance samples from the
  * ML-Plan search space for datasets from OpenML. Therefore a
@@ -27,8 +35,8 @@ import jaicore.search.model.probleminputs.GraphSearchInput;
  *
  */
 public class PerformanceSampleGenerator {
-	
-	public static void main(String args[]) {
+
+	public static void main(String[] args) {
 		String openmlID = args[0];
 		PerformanceSampleGenerator psg = new PerformanceSampleGenerator();
 		psg.evaluate(openmlID);
@@ -45,17 +53,18 @@ public class PerformanceSampleGenerator {
 		try {
 			SQLAdapter adapter = new SQLAdapter(config.getDBHost(), config.getDBUsername(), config.getDBPassword(),
 					config.getDBDatabaseName());
-			PerformanceDBAdapter performanceDBAdapter = new PerformanceDBAdapter(adapter, config.getDBTableName());
-
+			
 			ZeroOneLoss lossFunction = new ZeroOneLoss();
-			CacheEvaluatorMeasureBridge bridge = new CacheEvaluatorMeasureBridge(lossFunction, performanceDBAdapter);
-			MLPlanWekaClassifier mlplan = new WekaMLPlanWekaClassifier();
+			ObjectMapper mapper = new ObjectMapper();
+			
+			//TODO jonas: insert components here
+			MLPlanWekaClassifier mlplan = new MLPlanWekaMLPlanWekaClassifier();
 			ReproducibleInstances data = ReproducibleInstances.fromOpenML(openmlID, config.getOpenMLKey());
-			data.setCacheLookup(true);
-			data.setCacheStorage(true);
+
 			data.setClassIndex(data.numAttributes() - 1);
 			mlplan.setLoggerName("mlplan");
 			mlplan.setData(data);
+			
 			GraphGenerator gg = mlplan.getGraphGenerator();
 			GraphSearchInput gsi = new GraphSearchInput(gg);
 			RandomSearch rs = new RandomSearch(gsi, config.getRandomSearchSeed());
@@ -67,13 +76,21 @@ public class PerformanceSampleGenerator {
 						true);
 				try {
 					MLPipeline mlp = factory.getComponentInstantiation(ci);
-					CacheEvaluatorMeasureBridge shallowBridge = ((CacheEvaluatorMeasureBridge) bridge)
-							.getShallowCopy(ci);
-					MonteCarloCrossValidationEvaluator evaluator = new MonteCarloCrossValidationEvaluator(shallowBridge,
-							config.getNumberMCCVRepeats(), data, config.getMCCVTrainRatio(), config.getMCCVSeed());
+					MonteCarloCrossValidationEvaluator evaluator = new MonteCarloCrossValidationEvaluator(
+							new SimpleEvaluatorMeasureBridge(lossFunction), config.getNumberMCCVRepeats(), data,
+							config.getMCCVTrainRatio(), config.getMCCVSeed());
 					double score = evaluator.evaluate(mlp);
-					System.out.println(mlp);
-					System.out.println("score: " + score);
+					
+					
+					//insert into DB
+					Map<String, String> valueMap = new HashMap<>();
+					valueMap.put("composition", mapper.writeValueAsString(ci));
+					valueMap.put("dataset", openmlID);
+					valueMap.put("evaluation_date",
+							new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date.from(Instant.now())));
+					valueMap.put("score", Double.toString(score));
+					adapter.insert(config.getDBTableName(), valueMap);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
