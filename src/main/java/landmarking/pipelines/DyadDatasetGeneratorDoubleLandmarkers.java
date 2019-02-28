8d7e74c9a1a544d3e8ba141d0aaa7c6e84ddfa77
Jonas Hanselle <jmhansel@mail.uni-paderborn.de>
2019-02-28 23:38:34 +0100

    Work on landmarking dataset generator

--- src/main/java/landmarking/pipelines/DyadDatasetGeneratorDoubleLandmarkers.java
+++ src/main/java/landmarking/pipelines/DyadDatasetGeneratorDoubleLandmarkers.java
@@ -6,13 +6,11 @@ import java.io.FileOutputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
-
-import org.nd4j.linalg.primitives.Pair;
+import java.util.stream.Collectors;
 
 import com.google.common.primitives.Doubles;
 
@@ -21,6 +19,7 @@ import de.upb.isys.linearalgebra.Vector;
 import dyadranking.sql.SQLUtils;
 import jaicore.basic.SQLAdapter;
 import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
+import jaicore.ml.dyadranking.dataset.IDyadRankingInstance;
 import jaicore.ml.dyadranking.dataset.SparseDyadRankingInstance;
 
 public class DyadDatasetGeneratorDoubleLandmarkers {
@@ -76,42 +75,90 @@ public class DyadDatasetGeneratorDoubleLandmarkers {
 				e.printStackTrace();
 			}
 		}
-		
-		for (int datasetID : DATASET_IDS) {
-			List<Pair<Integer, Double>> idScorePairs = new ArrayList<Pair<Integer, Double>>();
-			for (int pipelineID : PIPELINE_IDS) {
-				try {
-					ResultSet rs = adapter.getResultsOfQuery(
-							"SELECT dataset_id, pipeline_id, loss FROM draco_pipeline_performance WHERE dataset_id = "
-									+ datasetID + " AND pipeline_ID = " + pipelineID);
-					while(rs.next()) {
-						idScorePairs.add(new Pair<Integer,Double>(rs.getInt(2), rs.getDouble(3)));
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 
+//		for (int datasetID : DATASET_IDS) {
+//			List<Pair<Integer, Double>> idScorePairs = new ArrayList<Pair<Integer, Double>>();
+//			for (int pipelineID : PIPELINE_IDS) {
+//				try {
+//					ResultSet rs = adapter.getResultsOfQuery(
+//							"SELECT dataset_id, pipeline_id, loss FROM draco_pipeline_performance WHERE dataset_id = "
+//									+ datasetID + " AND pipeline_ID = " + pipelineID);
+//					while (rs.next()) {
+//						idScorePairs.add(new Pair<Integer, Double>(rs.getInt(2), rs.getDouble(3)));
+//					}
+//				} catch (SQLException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//
+//			}
+//			Collections.sort(idScorePairs, Comparator.comparing(p -> p.getRight()));
+//			Vector instance = datasetMetafeatures.get(datasetID);
+//			List<Vector> alternatives = new ArrayList<Vector>();
+//			for (Pair<Integer, Double> pair : idScorePairs) {
+//				alternatives.add(pipelineMetafeatures.get(pair.getLeft()));
+////				System.out.println(
+////						"dataset: " + datasetID + " pipeline: " + pair.getLeft() + " loss: " + pair.getRight());
+////				System.out.println(
+////						"pipeline" + pair.getLeft() + " meta: " + " \t " + pipelineMetafeatures.get(pair.getLeft()));
+////				System.out.println("dataset" + datasetID + " meta: " + " \t " + datasetMetafeatures.get(datasetID));
+//			}
+//			SparseDyadRankingInstance drInstance = new SparseDyadRankingInstance(instance, alternatives);
+//			dataset.add(drInstance);
+//			try {
+//				FileOutputStream fos = new FileOutputStream(new File("landmarking_data_v2.txt"));
+//				dataset.serialize(fos);
+//			} catch (FileNotFoundException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//
+//		}
+		
+		List<Integer> pIDS = Arrays.stream(PIPELINE_IDS).boxed().collect(Collectors.toList());
+		List<Integer> dIDS = Arrays.stream(DATASET_IDS).boxed().collect(Collectors.toList());
+		
+		DyadRankingDataset ds = new DyadRankingDataset();
+		HashMap<Integer, ArrayList<Integer>> orderings = new HashMap<Integer, ArrayList<Integer>>();
+		try {
+			ResultSet reSet = adapter.getResultsOfQuery(
+					"SELECT dataset, pipeline_id, X_LANDMARKERS, pipeMeta, loss FROM dataset_metafeatures_mirror NATURAL JOIN (SELECT dataset_id as dataset, loss, pipeMeta, pipeline_id FROM (SELECT pipeline_id, COUNT(dataset_id) as cnt, GROUP_CONCAT(loss ORDER BY dataset_id DESC) as pipeMeta FROM `pipeline_landmarking_v2` GROUP BY pipeline_id) AS T NATURAL JOIN draco_pipeline_performance) AS NT ORDER BY dataset, loss");
+			while(reSet.next()) {
+				int dId = reSet.getInt(1);
+				int pId = reSet.getInt(2);
+				if(pIDS.contains(pId) && dIDS.contains(dId)) {
+				//				System.out.println(reSet.getInt(1) + " " + reSet.getInt(2) + " " + reSet.getString(4) + " " + reSet.getDouble(5));
+				if(!orderings.containsKey(dId))
+					orderings.put(dId, new ArrayList<Integer>());
+				orderings.get(dId).add(pId);
+				}
 			}
-			Collections.sort(idScorePairs, Comparator.comparing(p -> -p.getRight()));
-			Vector instance = datasetMetafeatures.get(datasetID);
-			List<Vector> alternatives = new ArrayList<Vector>();
-			for(Pair<Integer, Double> pair : idScorePairs) {
-				System.out.println(pair.getLeft() + " \t " + pipelineMetafeatures.get(pair.getLeft()));
-				alternatives.add(pipelineMetafeatures.get(pair.getLeft()));
-				System.out.println("dataset: " + datasetID + " pipeline: " + pair.getLeft() + " loss: " + pair.getRight());
-			}
-			SparseDyadRankingInstance drInstance = new SparseDyadRankingInstance(instance, alternatives);
-			dataset.add(drInstance);
-			try {
-				FileOutputStream fos = new FileOutputStream(new File("landmarking_data.txt"));
-				dataset.serialize(fos);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+		
+		System.out.println(orderings);
+		
+		for(int dID : orderings.keySet()) {
+			ArrayList<Integer> pipelines = orderings.get(dID);
+			ArrayList<Vector> alternatives = new ArrayList<Vector>();
+			Vector instance = datasetMetafeatures.get(dID);
+			for(int pID : pipelines) {
+				alternatives.add(pipelineMetafeatures.get(pID));
+			}
+			IDyadRankingInstance drInstance = new SparseDyadRankingInstance(instance, alternatives);
+			ds.add(drInstance);
+		}
+		System.out.println(ds);
+		try {
+			FileOutputStream fos = new FileOutputStream(new File("landmarking_data_v114.txt"));
+			ds.serialize(fos);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 //		System.out.println(dataset);
 	}
 
